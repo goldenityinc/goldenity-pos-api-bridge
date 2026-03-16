976f7f243cecf094426e93c75169cf20062c2c49
goldenityinc <goldenityinc@gmail.com>
2026-03-16 22:26:35 +0700

    fix: express wildcard preflight routing error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 
 app.use(publicRoutes);
