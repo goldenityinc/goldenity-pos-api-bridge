a24b4373765da43fc7104579bf6a7e96e33c2d96
goldenityinc <goldenityinc@gmail.com>
2026-04-17 18:01:09 +0700

    chore: add build marker to health endpoint for deployment verification

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -1,7 +1,7 @@
 const { jsonOk } = require('../utils/http');
 
 const healthCheck = (req, res) => {
-  return jsonOk(res, { status: 'ok' }, 'Bridge API healthy');
+  return jsonOk(res, { status: 'ok', build: 'dd6e729-cast-fix' }, 'Bridge API healthy');
 };
 
 module.exports = {
